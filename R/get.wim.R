f5b0eb7c7c80c0ae5c4b4ec823c0371e0af86e9d
James E. Marca <james@activimetrics.com>
2015-04-29 12:01:41 -0700

    older hack, get wim nozoo
    going to delete and refactor, but no need to delete this before
    committing it

--- R/get.wim.R
+++ R/get.wim.R
@@ -23,3 +23,21 @@ get.wim.imputed <- function(wim.site,year,direction,wim.path='/data/backup/wim')
     rm(df.wim.amelia.c,df.wim.amelia)
     df.zoo
 }
+
+get.wim.imputed.nozoo <- function(wim.site,year,direction,wim.path='/data/backup/wim'){
+    ## reload the imputed wim data
+    cdb.wimid <- paste('wim',wim.site,direction,sep='.')
+    savepath <- paste(wim.path,year,wim.site,direction,sep='/')
+    target.file <- make.amelia.output.file(savepath,paste('wim',wim.site,direction,sep=''),seconds,year)
+    print(paste('loading',target.file))
+    load.result <- load(file=target.file)
+    print(paste('load result is',load.result))
+    if(length(df.wim.amelia) == 1){
+        print(paste("amelia run for wim not good",df.wim.amelia))
+        return(1)
+    }else if(!length(df.wim.amelia)>0 || !length(df.wim.amelia$imputations)>0 || df.wim.amelia$code!=1 ){
+        print("amelia run for vds not good")
+        return(1)
+    }
+    df.wim.amelia
+}
