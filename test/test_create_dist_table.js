b06fae56db8f4d98a1b67654b8b4df435ce12aed
James E. Marca <james@activimetrics.com>
2015-05-13 09:06:12 -0700

    the distance table bit is passing
    the test demonstrates that the farthest pair that was used in 2009 data
    is 25,000+.
    
    The next set of "distant" pairs kicks in around 5000, then there are a
    lot (roughly 30) between 1000 and 5000

--- test/test_create_dist_table.js
+++ test/test_create_dist_table.js
@@ -15,6 +15,8 @@ var extract_couch_docs = require('../lib/extract_couch_docs.js')
 var queue = require('queue-async')
 var _ = require('lodash')
 
+var couch_check = require('couch_check_state')
+
 before(function(done){
 
     config_okay(config_file,function(err,c){
@@ -22,7 +24,6 @@ before(function(done){
             throw new Error('node.js needs a good croak module')
         }
         config = c
-        console.log(config)
         var q = queue()
         var opts =_.extend({'year':2012},c)
         q.defer(wim_imputed,opts)
@@ -30,7 +31,6 @@ before(function(done){
         q.await(function(e,w,v){
             config.wim_sites = w
             config.vds_sites = v
-
             return done()
 
         })
@@ -42,8 +42,41 @@ before(function(done){
 describe('query wim vds distances',function(){
     it('should get distance table for wim vds',function(done){
         create_dist_table(config,function(e,r){
-            console.log(r)
-            return done()
+            var numrecords = Object.keys(r).length
+            numrecords.should.equal(117)
+            var check_queue = queue(5)
+            var toolong = 0
+            Object.keys(r).forEach(function(wim_site){
+                // there is at least one site with distance 25,000+
+                // that was used as a pair in 2009
+                if(wim_site,r[wim_site][0].dist > 26000){
+                    toolong++
+                    check_queue.defer(function(cb){
+                        var opts = {
+                            'doc':wim_site
+                            ,'year':2009
+                            ,'state':'paired'
+                            ,'record':r[wim_site][0]
+                        }
+                        _.assign(opts,config.couchdb)
+                        couch_check(
+                            opts
+                            ,function(err,state){
+                                should.not.exist(err)
+                                should.exist(state)
+                                state.should.equal('none')
+                                return cb()
+
+                            })
+                        return null
+                    })
+                }
+                return null
+            })
+            check_queue.await(function(e,r){
+                toolong.should.equal(31)
+                return done(e)
+            })
 
         })
     })
